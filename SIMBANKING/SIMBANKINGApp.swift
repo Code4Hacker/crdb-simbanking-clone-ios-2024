9ebb1770096fb192c553d068a0acf242cd1268cd
Code4Hacker <black2022mail@gmail.com>
2024-04-01 13:51:49 +0300

    Initial Commit

---
+++ SIMBANKING/SIMBANKINGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIMBANKINGApp.swift
+//  SIMBANKING
+//
+//  Created by Gemini Child on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SIMBANKINGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
